0266b1d8b917f721c92a8444fb7ec63c1c155cd1
Emilio Farfan <n00495165@upn.pe>
2025-11-27 22:28:56 -0500

    Update PC.3.py

--- PC.3.py
+++ PC.3.py
@@ -1,1 +1,22 @@
-print("hola mundo")
+
+# Programa que pide 5 números y determina si son positivos, negativos o cero
+# Además, indica si son pares o impares
+
+for i in range(5):  # estructura repetitiva
+    numero = int(input(f"Ingrese el número {i+1}: "))
+
+    # Condicional anidada
+    if numero > 0:
+        print("Es positivo")
+        if numero % 2 == 0:
+            print("Además, es par")
+        else:
+            print("Además, es impar")
+    elif numero < 0:
+        print("Es negativo")
+        if numero % 2 == 0:
+            print("Además, es par")
+        else:
+            print("Además, es impar")
+    else:
+        print("Es cero")
